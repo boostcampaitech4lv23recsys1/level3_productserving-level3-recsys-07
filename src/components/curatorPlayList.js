c176997d3788875ba0f429e7cc9eb538af069abe
정승연_T4197 <jq3210@daum.net>
2023-01-15 09:51:13 +0900

    FEAT: 변수명 통일

--- src/components/curatorPlayList.js
+++ src/components/curatorPlayList.js
@@ -5,9 +5,9 @@ const CuratorPlayList = () =>{
     const style = "border-radius:12px";
 
     return(
-        <div className="playList">
-            <div className="playList_cls">
-                <iframe class="iframe_embed" style={{style}} src="https://open.spotify.com/embed/playlist/45iVWyXlQJp87zrwlX1j2M?utm_source=generator" 
+        <div className="curator_playlist">
+            <div className="playlist_cls">
+                <iframe className="iframe_embed" style={{style}} src="https://open.spotify.com/embed/playlist/45iVWyXlQJp87zrwlX1j2M?utm_source=generator" 
                     width="50%" height="352" frameBorder="0" allowFullScreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
         </div>
