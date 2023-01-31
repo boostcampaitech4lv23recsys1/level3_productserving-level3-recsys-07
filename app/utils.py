9a6425c38f9b15c2b755438c589ab192fceae4b7
KimBoseong <ses7720@example.com>
2023-01-31 07:36:25 +0000

    Feat: fix data index

--- app/utils.py
+++ app/utils.py
@@ -11,7 +11,7 @@ from typing import List, Union, Optional, Dict, Any
     
 def set_local_database():
     
-    song_meta_data = pd.read_csv("../data/song_meta_final.csv", sep=',', engine="pyarrow")
+    song_meta_data = pd.read_csv("../data/song_meta.csv", sep=',', engine="pyarrow")
     
     prename2id, id2track_name, id2url, id2artist, id2trackid, id2imgurl = {}, {}, {}, {}, {}, {}
     for track_name, url, id, artist, track_id, img_url in zip(song_meta_data.song_name, 
